6b3dd0fc3d3f00560e655bcd050b12d1c0bd8ca8
Evan Lu <evanlu7@gmail.com>
2018-01-29 15:46:14 -0800

    finished step 3

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-// current in stateless functional components
+
 
 var PLAYERS = [
 	{
@@ -31,21 +31,44 @@ Header.propTypes = {
 	title: React.PropTypes.string.isRequired,
 }
 
-function Counter (props){
-	return (
+var Counter = React.createClass({
+	propTypes: {
+		intialScore: React.PropTypes.number.isRequired,
+
+	},
+
+	getInitialState: function () {
+		return {
+			score: this.props.initialScore,
+		}
+	},
+
+	incrementScore: function(){
+		this.setState({
+			score: (this.state.score + 1),
+		});
+	},
+
+
+	decrementScore: function(){
+		this.setState({
+			score: (this.state.score - 1),
+		});
+	},
+
+
+
+	render: function () {
+		return(
 		<div className="counter">
-			<button className="counter-action decrement"> - </button>
-			<div className="counter-score">{props.score} </div>
-			<button className="counter-action increment"> + </button>
-				</div>
-				);
-}
+			<button className="counter-action decrement" onClick={this.decrementScore}>  - </button>
+			<div className="counter-score"> {this.state.score} </div>
+			<button className="counter-action increment" onClick={this.incrementScore}> + </button>
+		</div>
+		);
+	}
+});
 
-Counter.propTypes = {
-	score:React.PropTypes.number.isRequired,
-
-
-}
 
 
 
@@ -57,7 +80,7 @@ function Player (props){
 			{props.name}
 				</div>
 			<div className="player-score">
-				<Counter score={props.score} />
+				<Counter initialScore={props.score} />
 			</div>
 			</div>
 
